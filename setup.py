dd352245a241c1aa553db20da456a40488613605
Rajmund Csombordi <rajmund.csombordi@precisionformedicine.com>
2024-02-27 19:23:30 +0100

    Dependencies

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 
 setup(name='tomcru',
-      version='1.0.1',
+      version='1.0.0',
       description='Multi-purpose web framework',
       url='https://github.com/doorskgs/tomcru',
       author='oboforty',
@@ -11,4 +11,16 @@ setup(name='tomcru',
       zip_safe=False,
       packages=['tomcru'],
       install_requires=[
+            "prance>=0.22,<1.0",
+            "apispec>=6.3,<7.0",
+            "openapi-spec-validator>=0.5,<1.0",
+            "flask-swagger-ui>=4.11,<5.0",
+            "pyjwt[crypto]>=2.6,<3.0",
+            "requests>=2.28,<3.0",
+            "botocore>=1.29,<2.0",
+            "pyyaml==6.0",
+            "flatten-json==0.1.13",
+            "deepmerge==1.1.0",
+            "tabulate==0.9.0",
+            "tomcru-jerry[static]",
       ])
